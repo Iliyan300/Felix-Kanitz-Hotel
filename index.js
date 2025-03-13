db15d2f9aa94b0d54c5cb5e329d97145ace1389a
IliyanGeorgiev <iliangeorgiev300@yahoo.com>
2025-03-13 16:12:53 +0200

    modal update

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const modalCarousel = document.getElementById("img-carousel");
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+const modalImage = document.querySelector(".modal-image");
+
+const allViewBtns = document.querySelectorAll(".view-details");
+allViewBtns.forEach((btn) => {
+    btn.addEventListener("click", function() {
+
+        document.querySelector(".details-modal").style.display = "flex";
+    })
+})
+
+
+function getScrollAmount(image) {
+    const modalImageWidth = image.clientWidth;
+    const modalImageGap = parseInt(getComputedStyle(image).margin);
+    const scrollAmount = modalImageGap + modalImageWidth;
+    return scrollAmount;
+}
+
+
+document.getElementById("details-view").addEventListener("click", function() {
+    document.querySelector(".details-modal").style.display = "flex";
+})
+
+
+nextBtn.addEventListener("click", function() {
+    modalCarousel.scrollLeft += getScrollAmount(modalImage);
+})
+
+
+prevBtn.addEventListener("click", function() {
+    modalCarousel.scrollLeft -= getScrollAmount(modalImage);
+
+})
 
 
 
@@ -20,8 +56,6 @@ const changeBg = createBackgroundChanger("golfbar", [
     "images/golf bar/golf-bar-4.jpg",
     ]
 )
-
-
 
 
 document.getElementById("menu-icon").addEventListener("click", function() {
