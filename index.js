0d02a7ada94927cb4b26334d12913d8468de3a3e
IliyanGeorgiev <iliangeorgiev300@yahoo.com>
2025-03-18 14:54:05 +0200

    update font and modal appearance

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const prevBtn = document.getElementById("prev-btn");
 const closeBtn = document.getElementById("close-btn");
 const imageWrapper = document.getElementById("img-wrapper");
 const modalTitle = document.getElementById("modal-title");
-const modalRoomAmenities = document.getElementById("modal-amenities");
-const amenitiesList = modalRoomAmenities.querySelectorAll("span");
+const amenitiesList = document.getElementById("modal-amenities");
 const roomDescription = document.getElementById("rm-description");
+const facilitiesList = document.getElementById("modal-facilities")
 
     document.querySelectorAll(".view-details").forEach((btn) => {
         btn.addEventListener("click", function() {
@@ -19,12 +19,97 @@ const roomDescription = document.getElementById("rm-description");
             const apartment = apartmentsData.find((a => a.id === apartmentID));
             
             if(apartment) {
+                
+                /* DYNAMICALLY ADDING ICONS AND APARTMENT TEXT */
+                imageWrapper.innerHTML = ""  
+                amenitiesList.innerHTML = ""
+                facilitiesList.innerHTML = ""
+                modalTitle.textContent = apartment.name; 
+                roomDescription.textContent = apartment.description; 
+                let imageWidth; // <- storing image width value 
 
-                imageWrapper.innerHTML = ""
-                modalTitle.textContent = apartment.name;
-                roomDescription.textContent = apartment.description;
-                let imageWidth;
-               
+
+                apartment.facilities.forEach((facility) => {
+                    const liElement = document.createElement("li");
+                    const iconElement = document.createElement("i");
+                    
+
+                    switch(facility) {
+                        case "1 extra-large double bed":
+                        iconElement.className = "fa-solid fa-bed";
+                        break;
+                        case "1 sofa bed": 
+                        iconElement.className = "fa-solid fa-bed";
+                        break;
+                        case "1 double bed":
+                        iconElement.className = "fa-solid fa-bed";
+                        break;
+                        case "2 extra-large double beds":
+                        iconElement.className = "fa-solid fa-bed";
+                        break;
+                        default: 
+                        iconElement.className = "fa-solid fa-check";
+                        break;
+                    }
+
+                    const spanElement = document.createElement("span");
+                    spanElement.textContent = facility;
+                    
+                    liElement.append(iconElement, spanElement);
+                    facilitiesList.appendChild(liElement);
+
+            
+                })
+                
+                apartment.general.forEach((amenity) => {
+                    const liElement = document.createElement("li");
+                    const iconElement = document.createElement("i");
+                    
+                    // Adding icons according to the amenity //
+                    switch (amenity) {
+                        case "Entire apartment":
+                            iconElement.className = "fa-solid fa-house";
+                            break;
+                            case "Entire studio":
+                            iconElement.className = "fa-solid fa-house";
+                            break;
+                            case "Free WiFi":
+                            iconElement.className = "fa-solid fa-wifi";
+                            break;
+                            case "Private bathroom":
+                            iconElement.className = "fa-solid fa-shower";
+                            break;
+                            case "Private kitchen":
+                            iconElement.className = "fa-solid fa-utensils";
+                            break;
+                            case "Private kitchenette":
+                            iconElement.className = "fa-solid fa-utensils";
+                            break;
+                            case "Flat-screen TV":
+                            iconElement.className = "fa-solid fa-tv";
+                            break;
+                            case "Air conditioning":
+                            iconElement.className = "fa-solid fa-snowflake";
+                            break;
+                            case "Balcony":
+                            iconElement.className = "fa-solid fa-city";
+                            break;
+                            case "View":
+                            iconElement.className = "fa-solid fa-city";
+                            break;
+                        default:
+                            iconElement.className = "fa-solid fa-circle";
+                    }
+
+                    const spanElement = document.createElement("span");
+                    spanElement.textContent = amenity;
+
+                    liElement.append(iconElement, spanElement);
+                    amenitiesList.appendChild(liElement);
+                    
+                   
+                })
+
                 apartment.images.forEach((imgsrc) => {
                     const imgElement = document.createElement("img");
                     imgElement.classList.add("modal-image");
@@ -34,14 +119,14 @@ const roomDescription = document.getElementById("rm-description");
                 
                 
               
-                /* IMAGE CAROUSEL LOGIC */
+                /* Image Carousel Logic */
                 document.querySelector(".modal-image").addEventListener("load", function() {
-                    imageWidth = getScrollAmount(this);
+                    imageWidth = getScrollAmount(this); // <-- returns image width + gap value
                 })
 
             
                 nextBtn.addEventListener("click", function() {
-                    modalCarousel.scrollLeft += imageWidth;
+                    modalCarousel.scrollLeft += imageWidth; 
                 })
                 
 
